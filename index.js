576b6584fd8f3935b6bef80aa64cd242d03d3dba
Serega-Suschenko <sushenko.sergiy2003@gmail.com>
2022-02-16 23:57:43 +0200

    Add MyArray with push,pop,forEach,map

--- index.js
+++ index.js
@@ -1,95 +1,35 @@
-// const even = function(num){
-//  if (num % 2 === 0){
-//    return 'even'
-//  } else { return 'odd'}
-//}
-//const n = +prompt("Введите целое число.");
-//alert(even(n));
-//
-//
-//
-//const two = function(num1 , num2){
-//  if (num1 <= num2){return num1} else {return num2}
-//}
-//const x = +prompt("Введите число")
-//const y = +prompt ("Введите второе число")
-//alert (two(x,y))
-//
-//
-//
-//const money = function(num1 , num2){
-//  if (num1 >= num2){return 'Денег хватает'} else {return 'Денег мало'}
-//}
-//const x1 = +prompt("Количество денег")
-//const y2 = +prompt ("Введите сумму")
-//alert (money(x1,y2))
-
-function trafficlights(trafficcolor) {
-  const red = "red";
-  const yellow = "yellow";
-  const green = "green";
-  if (trafficcolor === red) {
-    return "Стоп";
-  } else if (trafficcolor === yellow) {
-    return "Приготовились";
-  } else if (trafficcolor === green) {
-    return "Гоу";
+class MyArray {
+  constructor() {
+    this.length = 0;
   }
+  push = function (value) {
+    this[this.length] = value;
+    
+    this.length++;
+    
+    return this.length;
+  };
+  pop = function () {
+    let del = this[this.length - 1];
+    
+    delete this[this.length - 1];
+    
+    this.length--;
+    
+    return del;
+  };
+  forEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i], i, this);
+    }
+  };
+  
+  map = function (callback) {
+    let result = new MyArray();
+    for (let i = 0; i < this.length; i++) {
+      let current = callback(this[i], i, this);
+      result.push(current);
+    }
+    return result;
+  };
 }
-
-//let num1 = Number(prompt("первое число"));
-//let num2 = Number(prompt("второе число"));
-//let operator = prompt("напиши оператор(/,+,*, -)");
-//function calc(num1, num2, operator) {
-//  let result;
-//  switch (operator) {
-//    case "-":
-//      result = num1 - num2;
-//      break;
-//    case "+":
-//      result = num1 + num2;
-//      break;
-//    case "*":
-//      result = num1 * num2;
-//      break;
-//    case "/":
-//      result = num1 / num2;
-//      break;
-//    default:
-//      alert("Такого оператора нет");
-//  }
-//  alert(result);
-//}
-//const result = calc(num1,num2,operator);
-
-function month(num) {
-  switch (num) {
-    case "12":
-    case "1":
-    case "2": {
-      console.log("Зима");
-      break;
-    }
-    case "3":
-    case "4":
-    case "5": {
-      console.log("Весна");
-      break;
-    }
-    case "6":
-    case "7":
-    case "8": {
-      console.log("Лето");
-      break;
-    }
-    case "9":
-    case "10":
-    case "11": {
-      console.log("Осень");
-      break;
-    }
-    default:{
-      console.log("Такого месяца нет")
-    }
-  }
-};
