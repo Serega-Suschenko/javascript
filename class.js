d145509156742c1a15e015af854e90ccf692f505
Serega_Suschenko <sushenko.sergiy2003@gmail.com>
2022-02-22 19:56:02 +0200

    Add recursive factorial and toPower

--- class.js
+++ class.js
@@ -1,41 +1,18 @@
-class Animal {
-  constructor(name, color) {
-    this.name = name;
-    this.color = color;
-    this.eat = true;
-    this.move = true;
+function factorial(number) {
+  if (number === 1) {
+    return number;
+  } else if (number < 0) {
+    throw new Error("number равен минусовому значению");
   }
- 
+  return number * factorial(n - 1);
 }
 
-class Lion extends Animal {
-  constructor(name, color) {
-    super(name, color);
-  }
-  getVoice(){
-    return "рык"
-  }
-}
-
-class Snake extends Animal {
-  constructor(name, color, lengthSnake) {
-    super(name, color,);
-    this.lengthSnake = lengthSnake;
-  }
-  getVoice(){
-    return "shhhhhhh"
+function toPower(number = 5, exp = 2) {
+  if (exp === 1) {
+    return number;
+  } else if (exp > 1) {
+    return number * toPower(number, exp - 1);
+  } else if (exp < 1) {
+    return 1 / (number / toPower(number, exp + 1));
   }
 }
-
-class Trainer {
-  constructor(name) {
-    this.name = name;
-  }
-  commandVoice(animal){
-    return animal.getVoice;
-  };
-}
-
-const lion = new Lion("sharik", "black");
-const trainer = new Trainer("Gari");
-const snake = new Snake("Billy", "green", 18);
